9c08fce2939587f67cbad79d558b8d053018ea7c
Eli Zevin <dizzyjaguar@gmail.com>
2020-03-02 17:06:53 -0800

    2 routes working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { 
+  Route, 
+  Switch,
+  Link,
+  BrowserRouter as Router,
+  Redirect, 
+} from 'react-router-dom';
 import Search from './Search';
+import Login from './Login';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Search/>
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Link to='/search/cool'>Look around</Link>
+        <Link to='/login'>Sign In Here </Link>
+        
+        <Switch>
+          <Route exact path='/search/cool' component={Search} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      
+
+      </Router>
+    )
+  }
 }
 
-export default App;
